ccf013b85b061eee678b746722ee343ef510997d
agusyudia <smkagus8@gmail.com>
2020-10-16 11:32:39 +0800

    jumat

---
+++ js/nav.js
@@ -0,0 +1,17 @@
+var tables ={[
+	'jeruk', '1 kg', 10000
+	],[
+	'apel', '1 kg', 20000
+	],[
+	'anggur', '1 kg', 50000
+	],[
+	'semangka', '1 kg', 150000
+	]
+};
+tables.foreach(function(val,index){
+	if (val[0] == 'jeruk') {
+		console.log('ketemu jeruknya');
+	}
+	console.log('coba dicarilagi..');
+		
+});
